55b63ec3ccca8fbb0150989c35ecb5b250f42d3a
Nikko Putra <nikko.putra@rocketmail.com>
2013-02-12 18:28:55 +0700

    meambahkan master topik

--- school-onlinetest-web/src/main/webapp/js/aplikasi.js
+++ school-onlinetest-web/src/main/webapp/js/aplikasi.js
@@ -14,6 +14,7 @@ angular.module('belajar', ['ui', 'belajar.controller'])
             .when('/master/peserta', {templateUrl: 'pages/master/peserta.html'})
             .when('/master/soal', {templateUrl: 'pages/master/soal.html'})
             .when('/master/nilai', {templateUrl: 'pages/master/nilai.html'})
+            .when('/master/topik', {templateUrl: 'pages/master/topik.html'})
             .when('/about', {templateUrl: 'pages/about.html', controller: 'AboutController'})
             .otherwise({templateUrl: 'pages/404.html'});
     }])
