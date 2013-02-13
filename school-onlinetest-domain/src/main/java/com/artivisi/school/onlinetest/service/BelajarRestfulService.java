44a9471e3af4afa19114120d9d07fbf980b1ad6f
Nikko Putra <nikko.putra@rocketmail.com>
2013-02-13 14:56:27 +0700

    menambahkan service Soal dan Pertanyaan

--- school-onlinetest-domain/src/main/java/com/artivisi/school/onlinetest/service/BelajarRestfulService.java
+++ school-onlinetest-domain/src/main/java/com/artivisi/school/onlinetest/service/BelajarRestfulService.java
@@ -5,8 +5,10 @@ import java.util.List;
 import com.artivisi.school.onlinetest.domain.ApplicationConfig;
 import com.artivisi.school.onlinetest.domain.Menu;
 import com.artivisi.school.onlinetest.domain.Permission;
+import com.artivisi.school.onlinetest.domain.Pertanyaan;
 import com.artivisi.school.onlinetest.domain.Peserta;
 import com.artivisi.school.onlinetest.domain.Role;
+import com.artivisi.school.onlinetest.domain.Soal;
 import com.artivisi.school.onlinetest.domain.User;
 import java.util.List;
 import org.springframework.data.domain.Page;
@@ -61,4 +63,18 @@ public interface BelajarRestfulService extends MonitoredService {
     Peserta findPesertaById(String id);
     Page<Peserta> findAllPesertas(Pageable pageable);
     Long countAllPesertas();
+    
+    // soal
+    void save(Soal soal);
+    void delete(Soal soal);
+    User findSoalById(String id);
+    Page<Soal> findAllSoals(Pageable pageable);
+    Long countAllSoals();
+    
+    // pertanyaan
+    void save(Pertanyaan pertanyaan);
+    void delete(Pertanyaan pertanyaan);
+    User findPertanyaanById(String id);
+    Page<Soal> findAllPertanyaans(Pageable pageable);
+    Long countAllPertanyaans();
 }
