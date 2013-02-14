11133912eedabe19f4169b5b6949015bacd74b58
lisa octavianie <lysa.mts@gmail.com>
2013-02-14 10:40:17 +0700

    menambhakan peserta untuk online

---
+++ school-onlinetest-web/src/main/java/com/artivisi/school/onlinetest/ui/controller/PesertaController.java
@@ -0,0 +1,100 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.artivisi.school.onlinetest.ui.controller;
+
+import com.artivisi.school.onlinetest.domain.Peserta;
+import com.artivisi.school.onlinetest.service.BelajarRestfulService;
+import java.awt.print.Pageable;
+import java.net.URI;
+import.java.until.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.util.UriTemplate;
+
+/**
+ *
+ * @author USER
+ */
+public class PesertaController {
+    @Autowired
+    private BelajarRestfulService belajarRestfulService;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    @RequestMapping("/user/{id}")
+    @ResponseBody
+    public Peserta findById(@PathVariable String id) {
+        Peserta x = belajarRestfulService.findPesertaById(id);
+        if (x == null) {
+            throw new IllegalStateException();
+        }
+    
+        
+        return x;
+
+    }
+    @RequestMapping(value = "/peserta", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody @Valid Peserta x, HttpServletRequest request, HttpServletResponse response) {
+        belajarRestfulService.save(x);
+        String requestUrl = request.getRequestURL().toString();
+        URI uri = new UriTemplate("{requestUrl}/{id}").expand(requestUrl, x.getId());
+        response.setHeader("Location", uri.toASCIIString());
+    }
+        @RequestMapping(method = RequestMethod.PUT, value="peserta/{id}" )
+        @ResponseStatus(HttpStatus.OK)
+        public void update (@PathVariable String id, @RequestBody @Valid Peserta x) {
+            Peserta p = belajarRestfulService.findPesertaById(id);
+            if (p == null){
+                logger.warn(" Peserta dengan id [{}] tidak ditemukan", id);
+                throw new IllegalStateException();  
+            }
+            belajarRestfulService.save(x);
+        }
+       @RequestMapping(method = RequestMethod.DELETE, value = "/soal/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable String id) {
+        Peserta p = belajarRestfulService.findPesertaById(id);
+        if (p == null) {
+            logger.warn("Peserta dengan id [{}] tidak ditemukan", id);
+            throw new IllegalStateException();
+        }
+        belajarRestfulService.delete(p);
+    }
+       @RequestMapping(value = "/soal", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Peserta> findAllPesertas(
+            Pageable pageable,
+            HttpServletResponse response) {
+        List<Peserta> hasil = belajarRestfulService.findAllPesertas(pageable).getContent();
+
+        return hasil;
+    }    
+        
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({IllegalStateException.class})
+    public void handle() {
+        logger.debug("Resource dengan URI tersebut tidak ditemukan");
+    }
+    }
+        
+            
+
+    
+
+   
+
+
